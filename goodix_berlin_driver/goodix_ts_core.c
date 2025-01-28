ea6a25f9a3fd9955305b4dccf07873b7dfccb95e
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2025-01-28 08:28:16 +0100

    Revert "touch-drivers: goodix_berlin_driver: Get rid of enable/disable_irq_wake"
    This reverts commit 9c94736983938c73e830ef2e76733f157ab867ba.
    
    Change-Id: I8b88bc82ca54bad7f722390e39b9888488087203

--- goodix_berlin_driver/goodix_ts_core.c
+++ goodix_berlin_driver/goodix_ts_core.c
@@ -1983,6 +1983,7 @@ static void goodix_set_gesture_work(struct work_struct *work)
 	target_gesture_type = core_data->nonui_enabled ? 0 : core_data->gesture_type;
 
 	if (target_gesture_type == 0) {
+		disable_irq_wake(core_data->irq);
 		hw_ops->irq_enable(core_data, false);
 		hw_ops->gesture(core_data, 0);
 		goto exit;
@@ -2002,6 +2003,7 @@ static void goodix_set_gesture_work(struct work_struct *work)
 		ts_err("enter gesture mode");
 	}
 	hw_ops->irq_enable(core_data, true);
+	enable_irq_wake(core_data->irq);
 
 exit:
 	pm_relax(core_data->bus->dev);
