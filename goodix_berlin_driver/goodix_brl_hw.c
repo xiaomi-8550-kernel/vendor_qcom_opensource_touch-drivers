f7056a0f6e0cf298967dc75c9beb8916658b6e15
AdarshGrewal <adarshgrewal@gmail.com>
2025-10-14 00:52:23 +0530

    WIP: touch-drivers: goodix_berlin_driver: Hook up edge filter modes
     * this is kinda part of xiaomis game mode this disables a bunch of stuff
     lowering the touch lag, hooked it into report rate toggle as only gamers
     care about this stuff
    
    Change-Id: I7254a42d8dc823cb8464b0ee6b246ac4555a6a42

--- goodix_berlin_driver/goodix_brl_hw.c
+++ goodix_berlin_driver/goodix_brl_hw.c
@@ -366,6 +366,67 @@ exit:
 	return ret;
 }
 
+#define GOODIX_GAME_CMD 0x17
+#define GOODIX_NORMAL_CMD	0x18
+static int brl_switch_edge_filter(struct goodix_ts_core *cd, bool enable)
+{
+	struct goodix_ts_cmd cmd;
+	int ret = 0;
+	const char *mode = enable ? "game" : "normal";
+
+	/*
+	this is how the data for game cmd works
+
+	data0 (0x00 = 00000000)
+
+	Bit 7 | Panel_Orientation | value: 0
+	Bit 6 | Panel_Orientation | value: 0
+	-------------------------------------------
+	Bit 5 | Touch_Tolerance | value: 0
+	Bit 4 | Touch_Tolerance | value: 0
+	Bit 3 | Touch_Tolerance | value: 0
+	-------------------------------------------
+	Bit 2 | Touch_UP_THRESHOLD | value: 0
+	Bit 1 | Touch_UP_THRESHOLD | value: 0
+	Bit 0 | Touch_UP_THRESHOLD | value: 0
+
+	data1 (0x00 = 00000000)
+
+	Bit 7 | Touch_Edge_Filter | value: 0
+	Bit 6 | Touch_Edge_Filter | value: 0
+	-------------------------------------------
+	Bit 5 | Touch_Aim_Sensitivity | value: 0
+	Bit 4 | Touch_Aim_Sensitivity | value: 0
+	Bit 3 | Touch_Aim_Sensitivity | value: 0
+	-------------------------------------------
+	Bit 2 | Touch_Tap_Stability | value: 0
+	Bit 1 | Touch_Tap_Stability | value: 0
+	Bit 0 | Touch_Tap_Stability | value: 0
+	*/
+
+  if (enable) {
+		cmd.cmd = GOODIX_GAME_CMD;
+		cmd.len = 6;
+		cmd.data[0] = 0x00;
+		cmd.data[1] = 0x00;
+  } else {
+		cmd.cmd = GOODIX_NORMAL_CMD;
+		cmd.len = 6;
+		cmd.data[0] = 0x02;
+		cmd.data[1] = 0x80;
+	}
+	ret = cd->hw_ops->send_cmd(cd, &cmd);
+	if (ret < 0) {
+			ts_err("edge filter: failed to send %s cmd", mode);
+			goto exit;
+	}
+	ts_info("edge filter: %s mode", mode);
+	cd->edge_filter = enable;
+
+exit:
+	return ret;
+}
+
 int brl_resume(struct goodix_ts_core *cd)
 {
 	int ret = 0;
@@ -1589,6 +1650,7 @@ static struct goodix_ts_hw_ops brl_hw_ops = {
 	.get_capacitance_data = brl_get_capacitance_data,
 	.set_coor_mode = brl_set_coor_mode,
 	.switch_report_rate = brl_switch_report_rate,
+	.switch_edge_filter = brl_switch_edge_filter,
 };
 
 struct goodix_ts_hw_ops *goodix_get_hw_ops(void)
