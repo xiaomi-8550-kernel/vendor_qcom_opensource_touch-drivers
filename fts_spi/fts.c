e638af559797e34434944529d634b4dbb4988f1f
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2025-10-27 17:42:15 +0100

    fixup! touch-drivers: fts_spi: Implement high touch polling rate support
    Change-Id: Ie5b3f8e9cebbb6d14a5b050059726bec8660f9fb

--- fts_spi/fts.c
+++ fts_spi/fts.c
@@ -5621,6 +5621,8 @@ static int fts_get_mode_value(void *private, enum touch_mode mode)
 		return fts_info->fod_longpress_gesture_enabled;
 	case TOUCH_MODE_NONUI_MODE:
 		return fts_info->nonui_status;
+	case TOUCH_MODE_REPORT_RATE:
+		return fts_info->reprot_rate;
 	default:
 		logError(1, "handler got mode %d, not implemented", mode);
 		return -EINVAL;
